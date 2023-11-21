14bb67786087fa0abbe1da62dca76301695de804
marijana29 <marijana.kalicanin@yahoo.com>
2023-11-21 08:44:06 +0100

    update

--- random_quote/script.js
+++ random_quote/script.js
@@ -1,6 +1,5 @@
 const quoteContainer = document.querySelector("#quote-container");
 const quoteElement = document.createElement("p");
-// quoteElement.textContent = "";
 
 function fetchQuote() {
   fetch("https://dummy-apis.netlify.app/api/quote")
@@ -19,5 +18,4 @@ const quoteButton = document.createElement("button");
 quoteButton.innerText = "Get Random Quote";
 quoteButton.addEventListener("click", fetchQuote);
 
-// Append the button to the quoteContainer
 quoteContainer.appendChild(quoteButton);
